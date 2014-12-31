a2e342e7d0212a9527d8a41481e118c97799ef53
Meke Ladna <ladna.meke@andela.co>
2014-12-31 22:42:45 +0100

    Initializing repo for GH Pages

---
+++ js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+  var url = 'https://api.github.com/users/andela-lmeke/repos';
+   
+  var callback = function (json) {
+    var viewHTML = '<ul class="properties_list">';
+    console.log(json);
+    $.each(json, function (index, item) {
+      viewHTML += '<li class ="image_list">';
+      viewHTML += '<a href="' + item.html_url + '">';
+      viewHTML += '<img src="' + item.owner.avatar_url + '" class="property_img"></a>';
+      viewHTML += '<span class="price">' +'id: ' +item.id + '</span>';
+      viewHTML += '<div class="property_details">';
+      viewHTML += '<h1>' + '<a href="' + item.html_url +'" target="_blank">'+item.name + '</a></h1>';
+      viewHTML += '<h2>' + item.description + '</h2></div>';
+      viewHTML += '</li>';
+    });//end each
+
+    viewHTML += '</ul>'
+    $('#content').html(viewHTML);
+  }//end callback
+   $.getJSON(url, callback);
+});
+
+
+
+$(".more_listing_btn").click(function() {
+  var user = $(".login_btn").val();
+  console.log(user);
+  console.log("changing location");
+  console.log(user);
+  window.location = "https://github.com/" + user;
+});
+
