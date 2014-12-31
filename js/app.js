51cd83e0d2475aa7cc4e3bc799954cef9ed73772
Meke Ladna <ladna.meke@andela.co>
2015-01-01 00:48:40 +0100

    Added correct images for slideshow. Also added actual project images in the view

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  $('#content').html('<center><img src="img/loading.gif" alt="loading..."></center>');
   var url = 'https://api.github.com/users/andela-lmeke/repos';
    
   var callback = function (json) {
@@ -8,14 +9,15 @@ $(document).ready(function() {
     $.each(json, function (index, item) {
       viewHTML += '<li class ="image_list">';
       viewHTML += '<a href="' + item.html_url + '">';
-      viewHTML += '<img src="' + item.owner.avatar_url + '" class="property_img"></a>';
+      // viewHTML += '<img src="' + item.owner.avatar_url + '" class="property_img"></a>';
+      viewHTML += '<img src="img/' + item.name + '.png" class="property_img"></a>';
       viewHTML += '<span class="price">' +'id: ' +item.id + '</span>';
       viewHTML += '<div class="property_details">';
       viewHTML += '<h1>' + '<a href="' + item.html_url +'" target="_blank">'+item.name + '</a></h1>';
       viewHTML += '<h2>' + item.description + '</h2></div>';
       viewHTML += '</li>';
     });//end each
-
+    // '..img/' +item.name + '.png'
     viewHTML += '</ul>'
     $('#content').html(viewHTML);
   }//end callback
