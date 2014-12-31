15fcf2d468282b348998ffcb23da35df5c72d6ce
Meke Ladna <ladna.meke@andela.co>
2014-12-31 22:36:25 +0100

    first commit

---
+++ js/slides.js
@@ -0,0 +1,54 @@
+jQuery(document).ready(function ($) {
+        //Define an array of slideshow transition code
+        var _SlideshowTransitions = [
+        {$Duration:1500,x:0.5,$Cols:2,$ChessMode:{$Column:3},$Easing:{$Left:$JssorEasing$.$EaseInOutCubic},$Opacity:2,$Brother:{$Duration:1500,$Opacity:2}}
+        ];
+        var options = {
+            $AutoPlay: true,
+            $SlideshowOptions: {
+                    $Class: $JssorSlideshowRunner$,
+                    $Transitions: _SlideshowTransitions,
+                    $TransitionsOrder: 1,
+                    $ShowLink: true
+                }
+        };
+        var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+
+        //responsive code begin
+        //you can remove responsive code if you don't want the slider scales
+        //while window resizes
+        function ScaleSlider() {
+            var parentWidth = $('#slider1_container').parent().width();
+            if (parentWidth) {
+                jssor_slider1.$ScaleWidth(parentWidth);
+            }
+            else
+                window.setTimeout(ScaleSlider, 30);
+        }
+        //Scale slider after document ready
+        ScaleSlider();
+                                        
+        //Scale slider while window load/resize/orientationchange.
+        $(window).bind("load", ScaleSlider);
+        $(window).bind("resize", ScaleSlider);
+        $(window).bind("orientationchange", ScaleSlider);
+        //responsive code end
+
+    });
+
+
+// document.ready(function () {
+
+//  $('#menu').hover({
+
+// })//end hover
+
+// })// end ready
+
+//First Use jQuery to construct drop down menu
+
+
+
+//when menu is hovered
+//show nav links as lists ordered vertically ie display:block
+//hide links when menu is unhovered
